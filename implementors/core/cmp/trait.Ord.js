1d7c4fda31b9ffa791814d74e3e14f46733d6ecc
JP-Ellis <coujellis@gmail.com>
2015-03-03 08:31:48 +0000

    Travis build 3 pushed to gh-pages.

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors['log'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='log/enum.LogLevel.html' title='log::LogLevel'>LogLevel</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='log/enum.LogLevelFilter.html' title='log::LogLevelFilter'>LogLevelFilter</a>",];
 implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='skiplist/struct.SkipList.html' title='skiplist::SkipList'>SkipList</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors['rand'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];
 implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='skiplist/struct.SkipList.html' title='skiplist::SkipList'>SkipList</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
