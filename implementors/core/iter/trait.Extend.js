c413cb1f3385624663dcf28d05191167787ed87d
JP-Ellis <coujellis@gmail.com>
2015-03-03 13:26:54 +0000

    Updated to commit 1f53e0047b530ab457013511b529151efc6cdddd (Travis build 8).

--- implementors/core/iter/trait.Extend.js
+++ implementors/core/iter/trait.Extend.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;T&gt; for <a class='struct' href='skiplist/struct.SkipList.html' title='skiplist::SkipList'>SkipList</a>&lt;T&gt;",];
+implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;T&gt; for <a class='struct' href='skiplist/struct.OrderedSkipList.html' title='skiplist::OrderedSkipList'>OrderedSkipList</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
