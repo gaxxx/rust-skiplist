5f82fc9198e0485521acea662a991c761436306c
JP-Ellis <coujellis@gmail.com>
2015-03-04 07:04:50 +0000

    Updated to commit 5cef0025535d3d6bebc251e0c295587517b2a940 (Travis build 9).

--- implementors/core/iter/trait.FromIterator.js
+++ implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;T&gt; for <a class='struct' href='skiplist/struct.OrderedSkipList.html' title='skiplist::OrderedSkipList'>OrderedSkipList</a>&lt;T&gt;",];
+implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;T&gt; for <a class='struct' href='skiplist/struct.OrderedSkipList.html' title='skiplist::OrderedSkipList'>OrderedSkipList</a>&lt;T&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(K, V)</a>&gt; for <a class='struct' href='skiplist/struct.SkipMap.html' title='skiplist::SkipMap'>SkipMap</a>&lt;K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/ops/trait.IndexMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='skiplist/struct.SkipMap.html' title='skiplist::SkipMap'>SkipMap</a>&lt;K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
