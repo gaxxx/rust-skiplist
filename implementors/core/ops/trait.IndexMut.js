01ba7530a88254d0dd4ccfbedf51847561cc5fdc
JP-Ellis <coujellis@gmail.com>
2015-04-27 06:32:35 +0000

    Updated to commit da33a57eca8623b5ddc1813b0915cb8b1e7ff7ab (Travis build 17).

--- implementors/core/ops/trait.IndexMut.js
+++ implementors/core/ops/trait.IndexMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['skiplist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='skiplist/struct.SkipMap.html' title='skiplist::SkipMap'>SkipMap</a>&lt;K, V&gt;",];
+implementors['skiplist'] = ["impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='skiplist/struct.SkipMap.html' title='skiplist::SkipMap'>SkipMap</a>&lt;K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
